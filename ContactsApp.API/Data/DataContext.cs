3a218ecc861f7829901b5979141d326a633e3c83
Admasu <tadmasu@kpmg.com>
2019-01-11 11:23:48 -0500

    Initilal Commit

---
+++ ContactsApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using ContactsApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
